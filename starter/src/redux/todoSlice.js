bb65118c76c90f92454d8987762d9d143d6caa9b
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-29 17:24:24 +0200

    Imported new addTodoAsync and dispatched (that action)

--- starter/src/redux/todoSlice.js
+++ starter/src/redux/todoSlice.js
@@ -11,6 +11,24 @@ export const getTodosAsync = createAsyncThunk(
   }
 );
 
+export const addTodoAsync = createAsyncThunk(
+  "todos/add/Todo/Async",
+  async (payload) => {
+    const response = await fetch("http://localhost:7000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: payload.title }),
+    });
+
+    if (response.ok) {
+      const todo = await response.json();
+      return { todo };
+    }
+  }
+);
+
 const todoSlice = createSlice({
   name: "todos",
   initialState: [],
@@ -35,6 +53,9 @@ const todoSlice = createSlice({
     [getTodosAsync.fulfilled]: (state, action) => {
       return action.payload.todos;
     },
+    [addTodoAsync.fulfilled]: (state, action) => {
+      state.push(action.payload.todo);
+    }
   },
 });
 
